155a645e4e87d7e1d6d3ead1e46d101498f1267c
sojung <sojung528@gmail.com>
2023-02-09 15:15:46 +0900

    View Controller 세팅

---
+++ src/main/java/shop/mtcoding/blog2/controller/UserController.java
@@ -0,0 +1,23 @@
+package shop.mtcoding.blog2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "user/loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "user/joinForm";
+    }
+
+    @GetMapping("/user/updateForm")
+    public String updateForm() {
+        return "user/updateForm";
+    }
+}
