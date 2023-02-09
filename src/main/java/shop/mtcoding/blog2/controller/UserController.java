c9e53683fc28eafd742704a730ba363550d401dc
sojung <sojung528@gmail.com>
2023-02-09 15:48:13 +0900

    로그인 완료

--- src/main/java/shop/mtcoding/blog2/controller/UserController.java
+++ src/main/java/shop/mtcoding/blog2/controller/UserController.java
@@ -1,23 +1,43 @@
 package shop.mtcoding.blog2.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import shop.mtcoding.blog2.dto.user.UserReq.JoinReqDto;
+import shop.mtcoding.blog2.dto.user.UserReq.LoginReqDto;
 import shop.mtcoding.blog2.handler.ex.CustomException;
+import shop.mtcoding.blog2.model.User;
 import shop.mtcoding.blog2.service.UserService;
 
 @Controller
 public class UserController {
 
     @Autowired
+    HttpSession session;
+    @Autowired
     UserService userService;
 
     @GetMapping("/loginForm")
     public String loginForm() {
         return "user/loginForm";
+    }
+
+    @PostMapping("/login")
+    public String login(LoginReqDto loginReqDto) {
+        if (loginReqDto.getUsername() == null || loginReqDto.getUsername().isEmpty()) {
+            throw new CustomException("username을 작성해주세요");
+        }
+        if (loginReqDto.getPassword() == null || loginReqDto.getPassword().isEmpty()) {
+            throw new CustomException("password를 작성해주세요");
+        }
+        User principal = userService.로그인(loginReqDto);
+
+        session.setAttribute("principal", principal);
+        return "redirect:/";
     }
 
     @GetMapping("/joinForm")
