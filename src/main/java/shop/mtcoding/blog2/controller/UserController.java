068799550c83abffc049f64864f334250b64ae35
sojung <sojung528@gmail.com>
2023-02-09 15:34:18 +0900

    회원가입 구현 완료

--- src/main/java/shop/mtcoding/blog2/controller/UserController.java
+++ src/main/java/shop/mtcoding/blog2/controller/UserController.java
@@ -1,10 +1,19 @@
 package shop.mtcoding.blog2.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import shop.mtcoding.blog2.dto.user.UserReq.JoinReqDto;
+import shop.mtcoding.blog2.handler.ex.CustomException;
+import shop.mtcoding.blog2.service.UserService;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    UserService userService;
 
     @GetMapping("/loginForm")
     public String loginForm() {
@@ -14,6 +23,22 @@ public class UserController {
     @GetMapping("/joinForm")
     public String joinForm() {
         return "user/joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(JoinReqDto joinReqDto) {
+        if (joinReqDto.getUsername() == null || joinReqDto.getUsername().isEmpty()) {
+            throw new CustomException("username을 작성해주세요");
+        }
+        if (joinReqDto.getPassword() == null || joinReqDto.getPassword().isEmpty()) {
+            throw new CustomException("password를 작성해주세요");
+        }
+        if (joinReqDto.getEmail() == null || joinReqDto.getEmail().isEmpty()) {
+            throw new CustomException("email을 작성해주세요");
+        }
+
+        userService.회원가입(joinReqDto);
+        return "redirect:/loginForm";
     }
 
     @GetMapping("/user/updateForm")
---
+++ src/main/java/shop/mtcoding/blog2/dto/user/UserReq.java
@@ -0,0 +1,14 @@
+package shop.mtcoding.blog2.dto.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class UserReq {
+    @Getter
+    @Setter
+    public static class JoinReqDto {
+        private String username;
+        private String password;
+        private String email;
+    }
+}
