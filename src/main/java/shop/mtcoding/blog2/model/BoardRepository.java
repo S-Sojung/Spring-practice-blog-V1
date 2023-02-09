e4633d2b2724db4cb57c14a723d4e44e04a6f9db
sojung <sojung528@gmail.com>
2023-02-09 16:50:58 +0900

    글 목록보기 테스트 완료

--- src/main/java/shop/mtcoding/blog2/model/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog2/model/BoardRepository.java
@@ -5,12 +5,16 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import shop.mtcoding.blog2.dto.board.BoardResp.BoardMainRespDto;
+
 @Mapper
 public interface BoardRepository {
         public List<Board> findAll();
 
         public Board findById(int id);
 
+        public List<BoardMainRespDto> findAllWithUser();
+        
         public int insert(@Param("title") String title, @Param("content") String content,
                         @Param("userId") int userId);
 
